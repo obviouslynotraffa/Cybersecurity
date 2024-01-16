6221f4b63b2f68559149e3f02b1759b9d5c8349f
raffaele <raffaele.fontana.02@gmail.com>
2024-01-16 20:46:52 +0100

    update README.md

---
+++ 6_exams/Crypto_Web/CryptoChallenge_5/README.md
@@ -0,0 +1,24 @@
+# Crypto Challenge
+
+### 📄 Description
+
+As usual, you intercepted a communication between the Professor and his assistants.
+
+Unfortunatly, the communication is encrypted and you can't read the content.
+
+Still, you found the file that generated the encrypted string!
+
+Reverse it and try do decrypt what they wrote!
+
+
+## 🔑 Solution
+
+We just need to reverse the order of the functions, so:
+1. We `XOR()` the encrypt message with the key, so we get a `32base` encryption 
+2. We convert the `32base` to ascii
+
+
+### 🚩 Flag
+```plain
+spritz{b4se32_is_as_secure_AS_bas364}
+```
---
+++ 6_exams/Crypto_Web/CryptoChallenge_5/sol.py
@@ -0,0 +1,25 @@
+
+import base64
+import string
+import binascii
+
+ALPHABET = list(string.printable)   # len = 100
+LEN = len(ALPHABET)
+
+def base32decode(message):
+    return base64.b32decode(message).decode('ascii')
+
+
+def XORdecode(message, KEY="Hug0"):
+    rep = len(message)//len(KEY) + 1
+    key = (KEY*rep)[:len(message)] # adjust the key len
+    xored = ''.join([chr(ord(a) ^ ord(b)) for a,b in zip(message, key)])
+    return xored
+
+with open('encrypted_flag', 'rb') as f:
+    encrypted_flag = f.read().decode('ascii')
+    f.close()
+    
+decoded = XORdecode(encrypted_flag)
+decoded = base32decode(decoded)
+print(decoded)
